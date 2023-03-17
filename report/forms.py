be095857eb480ac187f3b18957ba16f298e06677
Robert Scruggs <scruggstyrese@gmail.com>
2023-03-16 22:51:32 -0400

    reorganized database

--- report/forms.py
+++ report/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+#this is basically your html
+
 #class is inheriting from forms.Form
 class BasicInformation(forms.Form):
     financeReview = forms.CharField(label="Proposed Project for Financing Review", max_length=200)
@@ -14,6 +16,33 @@ class YearOne(forms.Form):
     area = forms.CharField(label="What state does your business operate out of", max_length=200)
     numOfLocations = forms.CharField(label="Please type number of locations for year 1", max_length=200)
     totalSales = forms.CharField(label="Total sales for year 1", max_length=200)
+    foodCost = forms.CharField(label="Food Cost", max_length=200)
+    laborCost = forms.CharField(label="Labor Cost", max_length=200)
+    adminAndGeneral = forms.CharField(label="Administrative & General Costs", max_length=200)
+    randsMarketing = forms.CharField(label="Royalty/Sales Marketing Costs", max_length=200)
+    incomeBeforeFixExpense = forms.CharField(label="Income Before Fix Expense", max_length=200)
+    propertyTax = forms.CharField(label="Property Tax", max_length=200)
+    insurance = forms.CharField(label="Insurance", max_length=200)
+    reserve = forms.CharField(label="Reserve", max_length=200)
+
+class YearTwo(forms.Form):
+    area = forms.CharField(label="What state does your business operate out of", max_length=200)
+    numOfLocations = forms.CharField(label="Please type number of locations for year 2", max_length=200)
+    totalSales = forms.CharField(label="Total sales for year 2", max_length=200)
+    foodCost = forms.CharField(label="Food Cost", max_length=200)
+    laborCost = forms.CharField(label="Labor Cost", max_length=200)
+    adminAndGeneral = forms.CharField(label="Administrative & General Costs", max_length=200)
+    randsMarketing = forms.CharField(label="Royalty/Sales Marketing Costs", max_length=200)
+    incomeBeforeFixExpense = forms.CharField(label="Income Before Fix Expense", max_length=200)
+    propertyTax = forms.CharField(label="Property Tax", max_length=200)
+    insurance = forms.CharField(label="Insurance", max_length=200)
+    reserve = forms.CharField(label="Reserve", max_length=200)
+
+
+class YearThree(forms.Form):
+    area = forms.CharField(label="What state does your business operate out of", max_length=200)
+    numOfLocations = forms.CharField(label="Please type number of locations for year 3", max_length=200)
+    totalSales = forms.CharField(label="Total sales for year 3", max_length=200)
     foodCost = forms.CharField(label="Food Cost", max_length=200)
     laborCost = forms.CharField(label="Labor Cost", max_length=200)
     adminAndGeneral = forms.CharField(label="Administrative & General Costs", max_length=200)
--- report/urls.py
+++ report/urls.py
@@ -2,8 +2,13 @@ from django.urls import path
 from . import views
 
 #URLConfig
+#when someone travels to one of these websites, they can choose from any of these endpoints
+#endpoints are linked to views which are linked to the forms
+#created using path objects
 urlpatterns = [
     path('', views.say_Hello),
     path('firstPage/', views.firstPage, name='firstPage'),
-    path('yearOne/', views.yearOne, name='yearOne')
+    path('yearOne/', views.yearOne, name='yearOne'),
+    path('yearTwo/', views.yearTwo, name='yearTwo'),
+    path('yearThree/', views.yearThree, name='yearThree')
 ]
