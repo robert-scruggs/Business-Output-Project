90c38d494cc3c90b0e368053b4eed6b0d8f2eb0b
Robert Scruggs <scruggstyrese@gmail.com>
2023-03-30 21:14:39 -0400

    got most of the code working for income tax return

--- report/practice2.py
+++ report/practice2.py
@@ -1,7 +1,7 @@
 import PyPDF2
 import pdfplumber
 
-listoffiles = ["6_2014 Income Tax Return_CUTTER RESTAURANT GROUP, LLC 1065 CLNT.pdf","6_2014_CUTTER HIGHLANDS RANCH, LLC 1065 CLNT.pdf"]
+listoffiles = ["6_2014 Income Tax Return_CUTTER RESTAURANT GROUP, LLC 1065 CLNT.pdf","6_2014_CUTTER HIGHLANDS RANCH, LLC 1065 CLNT.pdf","6_2015 Income Tax Return_ CUTTER RESTAURANT GROUP, LLC 1065 CLNT.pdf","6_2015 Income Tax Return_CUTTER HIGHLANDS RANCH, LLC 1065 CLNT.pdf"]
 myList = []
 pdf = pdfplumber.open("6_2014 Income Tax Return_CUTTER RESTAURANT GROUP, LLC 1065 CLNT.pdf")
 pdf2 = pdfplumber.open("6_2014_CUTTER HIGHLANDS RANCH, LLC 1065 CLNT.pdf")
@@ -17,16 +17,22 @@ for file in listoffiles:
                 print("found")
                 myList.append(page.extract_text())
                 break;
+ammortizationList = []
+counter = 1
+for x in range(1, len(myList), 2):
+     keepTrack = myList[counter].split(" ").index("44") + 1
+     amortization = myList[counter].split(" ")[keepTrack].split("\n")[0].replace(",","").replace(".","")
+     ammortizationList.append(amortization)
+     counter = counter + 2
 
-keepTrack = myList[1].split(" ").index("44") + 1
-keepTrack2 = myList[3].split(" ").index("44") + 1
-amortization = myList[1].split(" ")[keepTrack].split("\n")[0].replace(",","").replace(".","")
-amortization2 = myList[3].split(" ")[keepTrack2].split("\n")[0].replace(",","").replace(".","")
+for x in ammortizationList:
+    print(x)
 
+# keepTrack = myList[1].split(" ").index("44") + 1
+# keepTrack2 = myList[3].split(" ").index("44") + 1
+# amortization = myList[1].split(" ")[keepTrack].split("\n")[0].replace(",","").replace(".","")
+# amortization2 = myList[3].split(" ")[keepTrack2].split("\n")[0].replace(",","").replace(".","")
 
-
-
-print(amortization,amortization2)
 
 
 
